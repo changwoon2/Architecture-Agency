c98902f730af27f3d0de77d27570fe1562ba8668
changwoon2 <changwoon4@naver.com>
2022-06-13 00:46:26 +0900

    save

---
+++ custom.js
@@ -0,0 +1,22 @@
+$(function () {
+  $(".trigger").click(function () {
+    $(this).toggleClass("active");
+    $(".gnb").toggleClass("active");
+  });
+  $("section, .menu a").click(function () {
+    $(".gnb").removeClass("active");
+    $(".trigger").removeClass("active");
+  });
+  //     scrolling
+  $(".menu a, .gototop").click(function (e) {
+    $.scrollTo(this.hash || 0, 900);
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+      $("header, .gototop").addClass("active");
+    } else {
+      $("header, .gototop").removeClass("active");
+    }
+  });
+});
